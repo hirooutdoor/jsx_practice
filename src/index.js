89ef23a2a583ec188dc59a8db73abdaceb9ecab3
hiro <hmaeyama0510@gmail.com>
2021-05-08 13:08:03 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const YOURNAME = "Hirofumi Maeyama";
+const CURRENTYEAR = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {YOURNAME}. </p>
+    <p>Copyright {CURRENTYEAR}.</p>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
